ade199b1992317d962cc216e649bd3d936e77c07
franco1900 <misleyfranco2300@gmail.com>
2019-05-31 12:27:18 -0600

    cambio31/05

--- script.js
+++
@@ -1,9 +0,0 @@
-var pokemo= angular.module("pokemon",[])
-pokemon.controller("pokedex",function($Scope,$rootScope,$http){
-	$http({
-		method:"GET",
-		url:"https://pokeapi.co/api/v2/pokemon/25/"
-	}).then(function callbacksuccess(y){
-		console.log(y);
-	})
-})
